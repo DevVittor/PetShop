d5a2f9ed817eac6224b510658fee619b1c64b4d8
Vittor Fonseca Serra <102270411+DevVittor@users.noreply.github.com>
2024-04-12 14:35:37 -0300

    Update conn.js
    Alterando o mysql para ele funcionar em produção pelo render

--- database/conn.js
+++ database/conn.js
@@ -1,8 +1,15 @@
-import Sequelize from "sequelize";
+import mysql from 'mysql2';
 
-const conn = new Sequelize("pet","root","mint",{
+/*const conn = new Sequelize("pet","root","mint",{
     host:"localhost",
     dialect:"mysql"
+});*/
+
+const conn = mysql.createConnection({
+  host: 'seu_host_mysql',
+  user: 'seu_usuario_mysql',
+  password: 'sua_senha_mysql',
+  database: 'seu_banco_de_dados_mysql'
 });
 
 export default conn;
